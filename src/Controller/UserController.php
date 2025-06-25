c5ed0a7f50de00d4dd7fb45b2c658168977fd34a
aschlegle <antoine.schlegel01@gmail.com>
2025-06-25 11:17:03 +0200

    Supprimer activation de compte + paramètre et toute la logique de mot de passe et changement supprimé.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,8 +7,6 @@ use App\Entity\LogEvent;
 use App\Entity\Site;
 use App\Entity\User;
 use App\Entity\UsergroupMembership;
-use App\Form\UserEmailType;
-use App\Form\UserPasswordType;
 use App\Form\UserProfileType;
 use App\Security\UserVoter;
 use App\Service\Community;
@@ -94,314 +92,15 @@ class UserController extends AbstractController
 	}
 
 	/**
-	 * @Route("/user/register", name="user_register")
+	 * @Route("/user/profile/edit", name="user_profile_edit")
 	 *
-	 * @param \Symfony\Component\HttpFoundation\Request                               $request
-	 * @param \Doctrine\ORM\EntityManagerInterface                                   $manager
-	 * @param \App\Service\EmailSender                                              $mailer
-	 * @param \Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface $tokenGenerator
-	 * @param \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface   $passwordEncoder
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function register(
-		Request $request,
-		EntityManagerInterface $manager,
-		EmailSender $mailer,
-		TokenGeneratorInterface $tokenGenerator,
-		UserPasswordEncoderInterface $passwordEncoder
-	) {
-		if ($request->isMethod('POST') && ($request->request->get('action') === 'register')) {
-			// Start by capturing the reCAPTCHA response and verifying it
-			$recaptchaResponse = $request->request->get('g-recaptcha-response');
-			$recaptcha = new \ReCaptcha\ReCaptcha($this->recaptchaSecretKey);
-			$resp = $recaptcha->verify($recaptchaResponse, $request->getClientIp());
-
-			if (!$resp->isSuccess()) {
-				// If reCAPTCHA failed, flash an error message and redirect
-				$this->addFlash('error', 'Captcha validation failed, please try again.');
-				return $this->redirectToRoute('user_register');
-			}
-
-			// Check if the email already exists in the database
-			$userRepository = $manager->getRepository(User::class);
-			if ($userRepository->findOneBy(['email' => $request->request->get('email')])) {
-				$this->addFlash('error', 'messages.user.exists');
-				return $this->redirectToRoute('user_login');
-			}
-
-			if (!$request->request->get('agree_terms')) {
-				$this->addFlash('error', 'messages.user.not_agreed_terms');
-				return $this->redirectToRoute('user_login');
-			}
-
-			// Proceed to create user as reCAPTCHA is valid
-			$user = new User();
-			$user->setCreatedAt(new DateTime());
-			$user->setEmail($request->request->get('email'));
-			$user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
-			$user->setRoles([User::ROLE_USER]);
-			$user->setStatus(User::STATUS_PENDING);
-			$user->setHasAgreedTermsOfUse(true);
-
-			$token = $tokenGenerator->generateToken();
-			$user->setResetToken($token);
-
-			$manager->persist($user);
-
-			// Log Event
-			$log = new LogEvent();
-			$log->setType(LogEvent::USER_REGISTER);
-			$log->setUser($user);
-			$log->setCreatedAt(new \DateTime());
-			$manager->persist($log);
-
-			$manager->flush();
-
-			// Send activation email
-			$message = $this->renderView('emails/register-activation.html.twig', [
-				'user' => $user,
-				'url'  => $this->generateUrl('user_activate', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL),
-			]);
-
-			$mailer->send(
-				[$this->getParameter('plateform')['from'] => $this->getParameter('plateform')['name']],
-				$user->getEmail(),
-				$mailer->getSubjectFromTitle($message),
-				$message
-			);
-
-			$this->addFlash('notice', 'messages.user.activation_sent');
-			return $this->redirectToRoute('homepage');
-		}
-
-		return $this->redirectToRoute('user_login');
-	}
-
-
-	/**
-	 * @Route("/user/activate/{token}", name="user_activate")
-	 *
-	 * @param \Symfony\Component\HttpFoundation\Request                                           $request
-	 * @param string                                                                              $token
-	 * @param \Doctrine\ORM\EntityManagerInterface;                                               $manager
-	 * @param \Symfony\Component\HttpFoundation\Session\SessionInterface                          $session
-	 * @param \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface $tokenStorage
-	 * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface                         $eventDispatcher
-	 *
-	 * @param \App\Service\Community                                                              $community
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function activate(
-		Request $request,
-		string $token,
-		EntityManagerInterface $manager,
-		SessionInterface $session,
-		TokenStorageInterface $tokenStorage,
-		EventDispatcherInterface $eventDispatcher,
-		Community $community
-	) {
-		/**
-		 * @var $user User
-		 */
-		$user = $manager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
-
-		if ($user === NULL) {
-			$this->addFlash('error', 'messages.user.activation_token_unknown');
-
-			return $this->redirectToRoute('homepage');
-		}
-
-		if ($user->getStatus() === User::STATUS_ACTIVE) {
-			$this->addFlash('notice', 'messages.user.activation_already_active');
-
-			return $this->redirectToRoute('homepage');
-		}
-
-		if ($user->getStatus() !== User::STATUS_PENDING) {
-			$this->addFlash('warning', 'messages.user.activation_impossible');
-
-			return $this->redirectToRoute('homepage');
-		}
-
-		$user->setResetToken(NULL);
-		$user->setStatus(User::STATUS_ACTIVE);
-
-		// Join community
-
-		if ($community->getGroup()) {
-			$membership = new UsergroupMembership();
-			$membership->setUsergroup($community->getGroup());
-			$membership->setUser($user);
-			$membership->setRole(UsergroupMembership::ROLE_USER);
-			$membership->setStatus(UsergroupMembership::STATUS_MEMBER);
-			$membership->setJoinedAt(new \DateTime());
-
-			$manager->persist($membership);
-		}
-
-		//
-
-		$manager->flush();
-
-		// Manual login
-
-		$token = new UsernamePasswordToken($user, NULL, 'main', $user->getRoles());
-		$tokenStorage->setToken($token);
-		$session->set('_security_main', serialize($token));
-		$event = new InteractiveLoginEvent($request, $token);
-		$eventDispatcher->dispatch($event, 'security.interactive_login');
-
-		// Redirect to profile
-
-		$this->addFlash('notice', 'messages.user.activation_successful');
-
-		return $this->redirectToRoute('user_profile_create');
-	}
-
-	/**
-	 * @Route("/user/password", name="user_forgotten_password")
-	 *
-	 * @param \Symfony\Component\HttpFoundation\Request                               $request
-	 * @param \Doctrine\ORM\EntityManagerInterface;                                   $manager
-	 * @param \App\Service\EmailSender                                                $mailer
-	 * @param \Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface $tokenGenerator
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function forgottenPassword(
-		Request $request,
-		EntityManagerInterface $manager,
-		EmailSender $mailer,
-		TokenGeneratorInterface $tokenGenerator
-	) {
-		if ($request->isMethod('POST')) {
-			$email = $request->request->get('email');
-			/**
-			 * @var $user User
-			 */
-			$user = $manager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-			if ($user === NULL) {
-				$this->addFlash('warning', 'messages.user.unknown');
-
-				return $this->redirectToRoute('homepage');
-			}
-
-			if ($user->getStatus() !== User::STATUS_ACTIVE) {
-				$this->addFlash('error', 'messages.user.inactive');
-
-				return $this->redirectToRoute('homepage');
-			}
-
-			$token = $tokenGenerator->generateToken();
-
-			try {
-				$user->setResetToken($token);
-				$manager->flush();
-			} catch (Exception $e) {
-				$this->addFlash('warning', $e->getMessage());
-
-				return $this->redirectToRoute('homepage');
-			}
-
-			$message = $this->renderView('emails/forgotten-password.html.twig', [
-				'user' => $user,
-				'url'  => $this->generateUrl('user_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL),
-			]);
-
-			$mailer->send(
-				[$this->getParameter('plateform')['from'] => $this->getParameter('plateform')['name']],
-				$user->getEmail(),
-				$mailer->getSubjectFromTitle($message),
-				$message
-			);
-
-			$this->addFlash('notice', 'messages.user.password_sent');
-
-			return $this->redirectToRoute('homepage');
-		}
-
-		return $this->render('pages/user/password-forgotten.html.twig');
-	}
-
-	/**
-	 * @Route("/user/password/reset/{token}", name="user_reset_password")
-	 *
-	 * @param \Symfony\Component\HttpFoundation\Request                             $request
-	 * @param string                                                                $token
-	 * @param \Doctrine\ORM\EntityManagerInterface;                                 $manager
-	 * @param \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface $passwordEncoder
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function resetPassword(
-		Request $request,
-		string $token,
-		EntityManagerInterface $manager,
-		UserPasswordEncoderInterface $passwordEncoder
-	) {
-		if ($request->isMethod('POST')) {
-			/**
-			 * @var $user User
-			 */
-			$user = $manager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
-
-			if ($user === NULL) {
-				$this->addFlash('error', 'messages.user.password_token_unknown');
-
-				return $this->redirectToRoute('homepage');
-			}
-
-			if ($user->getStatus() !== User::STATUS_ACTIVE) {
-				$this->addFlash('error', 'messages.user.inactive');
-
-				return $this->redirectToRoute('homepage');
-			}
-
-			$user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
-			$user->setResetToken(NULL);
-			$manager->flush();
-
-			$this->addFlash('notice', 'messages.user.password_successful');
-
-			return $this->redirectToRoute('user_login');
-		} else {
-			return $this->render('pages/user/password-reset.html.twig', ['token' => $token]);
-		}
-	}
-
-	/**
-	 * @Route("/user/dashboard", name="user_dashboard")
-	 *
+	 * @param \Symfony\Component\HttpFoundation\Request  $request
 	 * @param \Doctrine\ORM\EntityManagerInterface;      $manager
+	 * @param \App\Service\FileManager                   $fileManager
 	 *
 	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
 	 */
-	public function dashboard(
-		EntityManagerInterface $manager
-	) {
-		$this->denyAccessUnlessGranted(UserVoter::LOGGED);
-
-		/**
-		 * @var User $user
-		 */
-		$user = $this->getUser();
-
-		$groups = array_map(function (UsergroupMembership $membership) {
-			return $membership->getUsergroup();
-		}, iterator_to_array($user->getUsergroupMemberships()));
-
-		$logEvents = $manager->getRepository(LogEvent::class)
-			->findForGroups($groups);
-
-		return $this->render('pages/user/dashboard.html.twig', ['logEvents' => $logEvents]);
-	}
-
-	protected function profileCreateEdit(
-		$template,
-		$confirmation,
+	public function profileEdit(
 		Request $request,
 		EntityManagerInterface $manager,
 		FileManager $fileManager
@@ -445,7 +144,6 @@ class UserController extends AbstractController
 
 				$user->setAvatar($file);
 			}
-			// --
 
 			// Convert Latitude et Longitude to Nuts code (european Region Code)
 			$latitude = $user->getLatitude();
@@ -459,7 +157,7 @@ class UserController extends AbstractController
 
 			$manager->flush();
 
-			$this->addFlash('notice', $confirmation);
+			$this->addFlash('notice', 'messages.user.profile_updated');
 
 			return $this->redirectToRoute('user_dashboard');
 		} else {
@@ -469,72 +167,30 @@ class UserController extends AbstractController
 			}
 		}
 
-		return $this->render($template, ['form' => $form->createView(), 'user' => $user]);
-	}
-
-	/**
-	 * @Route("/user/profile/create", name="user_profile_create")
-	 *
-	 * @param \Symfony\Component\HttpFoundation\Request  $request
-	 * @param \Doctrine\ORM\EntityManagerInterface;      $manager
-	 * @param \App\Service\FileManager                   $fileManager
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function profileCreate(
-		Request $request,
-		EntityManagerInterface $manager,
-		FileManager $fileManager
-	) {
-		return $this->profileCreateEdit(
-			'pages/user/profile-create.html.twig',
-			'messages.user.profile_created',
-			$request,
-			$manager,
-			$fileManager
-		);
-	}
-
-	/**
-	 * @Route("/user/profile/edit", name="user_profile_edit")
-	 *
-	 * @param \Symfony\Component\HttpFoundation\Request  $request
-	 * @param \Doctrine\ORM\EntityManagerInterface;      $manager
-	 * @param \App\Service\FileManager                   $fileManager
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function profileEdit(
-		Request $request,
-		EntityManagerInterface $manager,
-		FileManager $fileManager
-	) {
-		return $this->profileCreateEdit(
-			'pages/user/profile-edit.html.twig',
-			'messages.user.profile_updated',
-			$request,
-			$manager,
-			$fileManager
-		);
+		return $this->render('pages/user/profile-edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
 	}
 
 	/**
 	 * @Route("/user/parameters/edit", name="user_parameters_edit")
 	 *
-	 * @param \Symfony\Component\HttpFoundation\Request                               $request
-	 * @param \Doctrine\ORM\EntityManagerInterface;                                   $manager
-	 * @param \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface   $passwordEncoder
-	 * @param \Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface $tokenGenerator
-	 * @param \App\Service\EmailSender                                                $mailer
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function parametersEdit()
+	{
+		$this->denyAccessUnlessGranted(UserVoter::LOGGED);
+
+		return $this->render('pages/user/parameters-edit.html.twig');
+	}
+
+	/**
+	 * @Route("/user/dashboard", name="user_dashboard")
+	 *
+	 * @param \Doctrine\ORM\EntityManagerInterface;      $manager
 	 *
 	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
 	 */
-	public function parametersEdit(
-		Request $request,
-		EntityManagerInterface $manager,
-		UserPasswordEncoderInterface $passwordEncoder,
-		TokenGeneratorInterface $tokenGenerator,
-		EmailSender $mailer
+	public function dashboard(
+		EntityManagerInterface $manager
 	) {
 		$this->denyAccessUnlessGranted(UserVoter::LOGGED);
 
@@ -543,136 +199,19 @@ class UserController extends AbstractController
 		 */
 		$user = $this->getUser();
 
-		/**
-		 * @var User $emailUserSubmitted
-		 */
-		$emailUserSubmitted = new User();
-		$emailForm          = $this->createForm(UserEmailType::class, $emailUserSubmitted);
+		$groups = array_map(function (UsergroupMembership $membership) {
+			return $membership->getUsergroup();
+		}, iterator_to_array($user->getUsergroupMemberships()));
 
-		$vars = $request->request->get('user_email');
+		$logEvents = $manager->getRepository(LogEvent::class)
+			->findForGroups($groups);
 
-		if (!empty($vars['email_new'])) {
-			$emailForm->handleRequest($request);
-		}
-
-		if ($emailForm->isSubmitted() && $emailForm->isValid()) {
-			$userRepository = $manager->getRepository(User::class);
-
-			if (!$passwordEncoder->isPasswordValid($user, $emailUserSubmitted->getPassword())) {
-				$this->addFlash('error', 'messages.user.invalid_credentials');
-			} else if ($userRepository->findOneBy(['email' => $vars['email_new']])) {
-				$this->addFlash('error', 'messages.user.exists');
-			} else {
-				$token = $tokenGenerator->generateToken();
-
-				try {
-					$user->setEmailNew($vars['email_new']);
-					$user->setEmailToken($token);
-					$manager->flush();
-				} catch (Exception $e) {
-					$this->addFlash('warning', $e->getMessage());
-				}
-
-				// Send Warning
-
-				$message = $this->renderView('emails/email-change-warning.html.twig', [
-					'user' => $user,
-				]);
-
-				$mailer->send(
-					[$this->getParameter('plateform')['from'] => $this->getParameter('plateform')['name']],
-					$user->getEmail(),
-					$mailer->getSubjectFromTitle($message),
-					$message
-				);
-
-				// Send Confirmation
-
-				$message = $this->renderView('emails/email-change-confirm.html.twig', [
-					'user' => $user,
-					'url'  => $this->generateUrl('user_email_confirm', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL),
-				]);
-
-				$mailer->send(
-					[$this->getParameter('plateform')['from'] => $this->getParameter('plateform')['name']],
-					$vars['email_new'],
-					$mailer->getSubjectFromTitle($message),
-					$message
-				);
-
-				$this->addFlash('notice', 'messages.user.email_change_sent');
-			}
-		}
-
-		/**
-		 * @var User $passwordUserSubmitted
-		 */
-		$passwordUserSubmitted = new User();
-		$passwordForm          = $this->createForm(UserPasswordType::class, $passwordUserSubmitted);
-
-		$vars = $request->request->get('user_password');
-
-		if (!empty($vars['password_new'])) {
-			$passwordForm->handleRequest($request);
-		}
-
-		if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
-			if ($vars['password_new'] !== $vars['password_confirm']) {
-				$this->addFlash('error', 'messages.user.password_confirm_invalid');
-			} else if (!$passwordEncoder->isPasswordValid($user, $passwordUserSubmitted->getPassword())) {
-				$this->addFlash('error', 'messages.user.invalid_credentials');
-			} else {
-				$user->setPassword($passwordEncoder->encodePassword($user, $vars['password_new']));
-				$user->setResetToken(NULL);
-				$manager->flush();
-
-				$this->addFlash('notice', 'messages.user.password_updated');
-
-				return $this->redirectToRoute('user_dashboard');
-			}
-		}
-
-		return $this->render('pages/user/parameters-edit.html.twig', [
-			'emailForm'    => $emailForm->createView(),
-			'passwordForm' => $passwordForm->createView(),
-		]);
+		return $this->render('pages/user/dashboard.html.twig', ['logEvents' => $logEvents]);
 	}
 
-	/**
-	 * @Route("/user/email-change/{token}", name="user_email_confirm")
-	 *
-	 * @param string                                     $token
-	 * @param \Doctrine\ORM\EntityManagerInterface;      $manager
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function emailChangeConfirm(
-		string $token,
-		EntityManagerInterface $manager
-	) {
-		$this->denyAccessUnlessGranted(UserVoter::LOGGED);
 
-		/**
-		 * @var \App\Entity\User $user
-		 */
-		$user = $this->getUser();
 
-		$userRepository = $manager->getRepository(User::class);
 
-		if ($userRepository->findOneBy(['email' => $user->getEmailNew()])) {
-			$this->addFlash('error', 'messages.user.exists');
-		} else if (($token !== $user->getEmailToken()) || (empty($user->getEmailNew()))) {
-			$this->addFlash('error', 'messages.user.password_token_unknown');
-		} else {
-			$user->setEmail($user->getEmailNew());
-			$user->setEmailToken(NULL);
-			$manager->flush();
-
-			$this->addFlash('notice', 'messages.user.email_change_successful');
-		}
-
-		return $this->redirectToRoute('user_dashboard');
-	}
 
 	/**
 	 * @Route("/user/groups", name="user_groups")
@@ -714,11 +253,6 @@ class UserController extends AbstractController
 		 */
 		$user = $this->getUser();
 
-		if (User::STATUS_ACTIVE !== $user->getStatus()) {
-			$this->addFlash('error', 'messages.user.not_active');
-
-			return $this->redirectToRoute('homepage');
-		}
 		$userAnonymize->anonymize($user);
 		$manager->flush();
 
